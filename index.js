ec8bc9dc09eccbc0112e1291a3b459e4a86030b5
StasiyaN <ana.nyzhalska@gmail.com>
2025-05-29 05:23:07 +0200

    Ãlet const правка/правка вычислекнния 3ого примера

--- index.js
+++ index.js
@@ -1,40 +1,37 @@
 //первое задание
 //1
-let a = 2
+const  a = 2
 let result = a*2+a
 //результат первого примера
 console.log(result)
-// 1 + 2
-let b = 54
-let c = 16
+// 2
+const b = 54
+const c = 16
 // результат 2ого примера
-let result2 = (Math.sin(b)*Math.cos(c))**2
+const result2 = (Math.sin(b)*Math.cos(c))**2
 console.log(result2)
+//3
+const d = 16 * Math.sqrt(13.2 * 71.9)
+const e = 2.4 / 7 ** 4
+const g = 3 * Math.sqrt(49)
+const h =  2 ** 7
 
-let dResult = Math.sqrt(13.2 * 71.9)
-console.log(dResult)
-let eResult = 2.4/7**4
-let f = 3
-console.log(eResult)
-let totalResult = ((c*dResult)/eResult * f * Math.sqrt(49)) * 2**7
-console.log(totalResult)
-let finalResult = Math.round(totalResult)
-//результат 3его примера
-console.log(finalResult)
+const result3 = (d/e + g) * h
+//результат 3 примера
+console.log(result3)
+
 
 //2ое задание
-let number = 35
-let number2 = 186
-let number3 = 3
+const number = 35
+const number2 = 186
+const number3 = 3
 
 console.log(number%2 ===0) // не четное - false
 console.log(number2%2===0) // четное - true
 console.log(number3%2===0) // не четное - false
 
 // 3е задание
-let guest = 'Jason'
-if (!guest) {
-    console.log('Hello, guest!')
-} else {
-    console.log(`Hello, ${guest}!`)
-}
+let guest;
+guest = 'Jason'
+
+console.log(`Hello ${guest || `guest`}`)
